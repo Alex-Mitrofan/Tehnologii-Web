e87a35d983f8ec82e91bfdf75d7eea0910197571
Alex-Mitrofan <mitrofan.alex01@gmail.com>
2023-06-14 20:42:11 +0300

    Login

--- Manners Matter/controllers/login_controller.php
+++ Manners Matter/controllers/login_controller.php
@@ -4,15 +4,29 @@ session_start();
 if(isset($_COOKIE["username"]))
     header('Location: Home', true, 303);
 
-if (isset($_POST["username"]) && isset($_POST["password"]) && !empty($_POST["username"]) && !empty($_POST["password"])) 
-    if($_POST["username"] == "alex" && $_POST["password"] == "parola")
-    {
-        #echo $_POST["username"];
-        setcookie("username", $_POST["username"]);
-        $_SESSION["user"] = $_POST["username"];
-        header('Location: Home', true, 303);
-        
-    }
+     
+$HOSTNAME='localhost';
+$USERNAME='root';
+$PASSWORD='';
+$DATABASE='manners_matter';
+
+$con=mysqli_connect($HOSTNAME, $USERNAME, $PASSWORD, $DATABASE);
+
+if(isset($_POST["username"]) && isset($_POST["password"])){
+    $sql = "SELECT * FROM users where username='".$_POST["username"]."' and password='".$_POST["password"]."';";
+    $result = mysqli_query($con, $sql);
+
+
+    if (mysqli_num_rows($result) > 0) 
+        {
+            #echo $_POST["username"];
+            setcookie("username", $_POST["username"]);
+            $_SESSION["user"] = $_POST["username"];
+            header('Location: Home', true, 303);
+            
+        }
+
+}
 
 require '../views/login.html';
  
