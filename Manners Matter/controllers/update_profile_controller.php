9b315e2be6e619b4101068f7565fc13b1ebbe730
Alex-Mitrofan <mitrofan.alex01@gmail.com>
2023-06-18 23:36:07 +0300

    edit profile

--- Manners Matter/controllers/update_profile_controller.php
+++ Manners Matter/controllers/update_profile_controller.php
@@ -1,10 +1,19 @@
 <?php
-$username = '';
+require "../models/update_profile_model.php"; 
+ 
 
-
-
-if(isset($_POST["username"])) 
-    $new_username = $_POST["username"];
+if(isset($_POST["name"])) 
+    $name = $_POST['name'];
+if(isset($_POST["location"])) 
+    $location = $_POST['location'];
+if(isset($_POST["age"])) 
+    $age = $_POST['age'];
+if(isset($_POST["sex"])) 
+    $sex = $_POST['sex'];
+if(isset($_POST["ocupation"])) 
+    $ocupation = $_POST['occupation'];
+ 
+update_user($username, $name, $location, $age, $sex, $ocupation); 
     
  
 ?>
