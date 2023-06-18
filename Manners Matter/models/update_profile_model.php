3dd51a0335f5253aae49ca72caf7234a369df4b7
Alexandru Mitrofan <79212448+Alex-Mitrofan@users.noreply.github.com>
2023-06-19 00:58:47 +0300

    small bug update profile

--- Manners Matter/models/update_profile_model.php
+++ Manners Matter/models/update_profile_model.php
@@ -5,12 +5,7 @@ require 'database_connection.php';
 function update_user($username, $name, $location, $age, $sex, $ocupation ){
     global $con;
     $sql = "UPDATE users SET name = '$name', location = '$location', age = '$age', sex = '$sex', ocupation = '$ocupation'  WHERE username = '$username'";
-    $result = mysqli_query($con, $sql);
-
-    if (mysqli_num_rows($result)) 
-        return 1;
-
-    return 0;            
+    $result = mysqli_query($con, $sql);         
             
     }
 
